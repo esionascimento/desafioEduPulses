28468806e0743692ac06df7f25c4aaf2d131365b
esionascimento <esio.392@protonmail.com>
2021-11-06 06:31:00 -0400

    finalizado questao 1

--- questao02/index.js
+++ questao02/index.js
@@ -1,22 +1,23 @@
 function submit() {
   const name = document.getElementById('name').value;
   const date = document.getElementById('date').value.split("-");
-  console.log('date :', date);
   const dia = date[2];
   const mes = date[1];
   const ano = date[0];
   const city = document.getElementById('city').value;
-  console.log('name :', name);
-  console.log('dia :', dia);
-  console.log('mes :', mes);
-  console.log('ano :', ano);
-  console.log('city :', city);
   
   if (dia < 1 || dia > 31) {
     toastr["error"]("Erro: Data inválida")
   } else if (name === '' || city === '' || date.length <= 1) {
     toastr["error"]("Campo nome e cidade não preenchidos!");
   } else {
+    document.getElementById("campoNome").innerHTML = `Nome: ${name}`;
+    document.getElementById("campoData").innerHTML = `Data de nascimento: ${dia}/${mes}/${ano}`;
+    document.getElementById("campoCidade").innerHTML = `Cidade: ${city}`;
+
+    document.getElementById("name").value = '';
+    document.getElementById("date").value = '';
+    document.getElementById("city").value = '';
     $("#login-modal").modal('focus');
   }
 }
