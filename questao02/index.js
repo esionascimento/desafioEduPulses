b9b95d3e9fbe1fda3772d750128862e199e637bd
esionascimento <esio.392@protonmail.com>
2021-11-06 06:03:09 -0400

    create condiçao para inserir dados

--- questao02/index.js
+++ questao02/index.js
@@ -1,11 +1,4 @@
 function submit() {
-  console.log('entrei');
-  /* const aux = document.getElementById('login-modal');
-  const aux1 = document.getElementsByClassName('modal');
-  aux.addEventListener('shown.bs.modal', function() {
-    console.log('opa')
-    aux1.focus()
-  }) */
   const name = document.getElementById('name').value;
   const date = document.getElementById('date').value.split("-");
   console.log('date :', date);
@@ -13,14 +6,17 @@ function submit() {
   const mes = date[1];
   const ano = date[0];
   const city = document.getElementById('city').value;
-
-  /* const aux = document.getElementById('aModal');
-  aux = attribute('rel', 'modal:open') */
-  /* $('#aModal').modal('backdrop') */
+  console.log('name :', name);
+  console.log('dia :', dia);
+  console.log('mes :', mes);
+  console.log('ano :', ano);
+  console.log('city :', city);
+  
   if (dia < 1 || dia > 31) {
     toastr["error"]("Erro: Data inválida")
+  } else if (name === '' || city === '' || date.length <= 1) {
+    toastr["error"]("Campo nome e cidade não preenchidos!");
   } else {
-      console.log('oi')
-      $("#login-modal").modal('focus');
+    $("#login-modal").modal('focus');
   }
 }
